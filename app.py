377409ebf451bef2500b6c4311100efb3b9e9ced
henry mugai <106584669+HenryMugai@users.noreply.github.com>
2022-07-06 09:04:22 +0300

    text commit

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-import streamlit as st
-import panda as pd 
-import streamlit as st 
-
-
+ import streamlit as st 
 st.header('hello')
 st.text_area('top investments to consider')
 st.text_area('God is Good.')
