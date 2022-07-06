d2df25145c896218179a41f9a764d48d7f8a5d5e
henry mugai <106584669+HenryMugai@users.noreply.github.com>
2022-07-06 10:32:09 +0300

    commit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pandas
 
-st.header('WOMEN LED INVESTMENTS.')
-st.text_area('top investments to consider')
+st.tittle('TOP INVESTMENT IDEAS.')
+st.header('What is investment?')
+st.text_area('An asset acquired or invested in to build wealth and save money from the hard-earned income or appreciation.
+Primarily, it is obtaining an additional source of income or gain profit from the investment over a specific period of time.
+')
 st.text_area('God is Good.')
