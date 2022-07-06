10ea0564eb75d16a6d8b6f0e07e9582c60219086
henry mugai <106584669+HenryMugai@users.noreply.github.com>
2022-07-06 10:27:20 +0300

    commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pandas
 
-st.header('WOMEN LED INVESTMENTS.')
+####st.header('WOMEN LED INVESTMENTS.')
 st.text_area('top investments to consider')
 st.text_area('God is Good.')
