abb109cdabfcbe46b438684a53bd56d983b8e17f
henry mugai <106584669+HenryMugai@users.noreply.github.com>
2022-07-06 10:45:26 +0300

    commit

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pandas
 
 st.header('TOP INVESTMENT IDEAS.')
 st.header('What is investment?')
-st.text_area('An asset acquired or invested in to build wealth and save money from the hard-earned income or appreciation.Primarily, it is obtaining an additional source of income or gain profit from the investment over a specific period of time.')
+st.write('An asset acquired or invested in to build wealth and save money from the hard-earned income or appreciation.Primarily, it is obtaining an additional source of income or gain profit from the investment over a specific period of time.')
 st.header('Types of investments.')
-st.text_area('1.Growth investments')
-st.text_area ('They are more suitable for long term investors that are willing and able to withstand market ups and downs.')
+st.write('1.Growth investments')
+st.write ('They are more suitable for long term investors that are willing and able to withstand market ups and downs.')
 st.write('Shares are considered a growth investment as they can help grow the value of your original investment over the medium to long term.If you own shares, you may also receive income from dividends, which are effectively a portion of a company’s profit paid out to its shareholders.Of course, the value of shares may also fall below the price you pay for them. Prices can be volatile from day to day and shares are generally best suited to long term investors, who arecomfortable withstanding these ups and downs.Also known as equities, shares have historically delivered higher returns than other assets, shares are considered one of the riskiest types of investment.')
