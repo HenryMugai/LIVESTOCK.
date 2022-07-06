d91b2b01bf4943a4fc0112698098050b71089fd7
henry mugai <106584669+HenryMugai@users.noreply.github.com>
2022-07-06 10:41:38 +0300

    commit

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ st.text_area('An asset acquired or invested in to build wealth and save money fr
 st.header('Types of investments.')
 st.text_area('1.Growth investments')
 st.text_area ('They are more suitable for long term investors that are willing and able to withstand market ups and downs.')
+st.text('Shares are considered a growth investment as they can help grow the value of your original investment over the medium to long term.
+If you own shares, you may also receive income from dividends, which are effectively a portion of a company’s profit paid out to its shareholders.
+Of course, the value of shares may also fall below the price you pay for them. Prices can be volatile from day to day and shares are generally best suited to long term investors, who are comfortable withstanding these ups and downs.
+Also known as equities, shares have historically delivered higher returns than other assets, shares are considered one of the riskiest types of investment.
+')
