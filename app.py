f0023ca6bfaaa4f12a60c42ad147b5ff033e1328
henry mugai <106584669+HenryMugai@users.noreply.github.com>
2022-07-06 10:38:03 +0300

    commit

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ import pandas as pandas
 st.header('TOP INVESTMENT IDEAS.')
 st.header('What is investment?')
 st.text_area('An asset acquired or invested in to build wealth and save money from the hard-earned income or appreciation.Primarily, it is obtaining an additional source of income or gain profit from the investment over a specific period of time.')
-st.text_area('God is Good.')
+st.header('Types of investments.')
+st.text_area('1.Growth investments
+              These are more suitable for long term investors that are willing and able to withstand market ups and downs.')
